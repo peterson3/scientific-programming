e1549dbdcb6e23792bcaef26ba488bd56381a011
Peterson Andrade <tiago.peterson@gmail.com>
2017-11-29 19:11:03 -0200

    RK 4a ordem

--- js/app.js
+++ js/app.js
@@ -86,6 +86,44 @@
 	 	return result;
 	}
 
+	function metodoDeRungeKutta4(f, h, n){
+		
+		let x0, y0, x,y;
+		let i;
+		let xs = [];
+		let ys = [];
+		
+
+		/* Condicao inicial */
+		x0 = 0.0;
+		y0 = 1.0;
+
+		/* Inicializacao das variaveis */
+		x = x0;
+		y = y0;
+
+		for (i = 0; i < n; i++) 
+		    {
+
+		    xs.push(x);
+		    ys.push(y);
+
+		   	k1= f(x, y);
+		   	k2= f(x + h/2, y + (h/2)*k1);
+		   	k3= f(x + h/2, y + (h/2)*k2);
+		   	k4= f(x + h, y + h*k3);
+
+		    y = y + ((h/6)*(k1 + 2*k2 + 2*k3 + k4));
+		    x = x + h;
+		    }
+
+		let result = [];
+	 	result.push(xs);
+	 	result.push(ys);
+
+	 	return result;
+	}
+
 	function funcParser(){
 		// create a parser
 		var parser = math.parser();
@@ -116,12 +154,17 @@
 		        result = metodoDeHeun(f,h,n);
 		        plotXY(result, "Heun");
 		        break;
+		     case "Runge-Kutta 4a Ordem":
+		     	result = metodoDeRungeKutta4(f,h,n);
+		        plotXY(result, "Rk4");
+		        break;
 		    default:
 		        alert("Selecione um Método")
 		}
 		}
 		catch(exception){
 			alert("Erro: Verifique os Dados");
+			console.log(exception.message);
 		}
 	}
 
