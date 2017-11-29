2497ff4ec405df8a095daf227a91f639153636cd
Peterson Andrade <tiago.peterson@gmail.com>
2017-11-29 14:48:04 -0200

    Heun + minor changes

--- js/app.js
+++ js/app.js
@@ -1,8 +1,9 @@
-	function plotXY(methodResult){
+	function plotXY(methodResult, nomeMetodo){
 		PLOTAREA = document.getElementById('plotArea');
 		Plotly.plot( PLOTAREA, [{
 		x: methodResult[0],
-		y: methodResult[1] }], {
+		y: methodResult[1],
+		name: nomeMetodo }], {
 		margin: { t: 0 } } );
 
 	 	console.table(methodResult[0]);
@@ -12,12 +13,14 @@
 	function metodoDeEuler(f, h, n){
 		
 		console.log("Método de Euler");
+		
 		//variáveis de controle
 		let i,j;
 
 		//variáveis
 		let x0, y0, y, xfim, x, k1;
 
+		//dados do resultado a ser plotado
 		let xs=[];
 		let ys=[];
 
@@ -45,6 +48,44 @@
 	 	return result;
 	}
 
+	function metodoDeHeun(f, h, n){
+
+		console.log("Método de Heun");
+
+		//Variáveis de Controle
+		let i, j;
+
+		//Variáveis Inerentes
+		let x0, y0, y, x, k1, k2;
+	   
+	   	//dados do resultado a ser plotado
+		let xs=[];
+		let ys=[];
+
+	   //Condição Inicial
+		x0 = 0.0;
+		y0 = 1.0;
+		
+		//Definiando Próximas Iterações
+		x = x0;
+		y = y0;
+
+		for (i = 0; i < n; i++){
+		   xs.push(x);
+	 	   ys.push(y);
+		   k1 = f(x, y);
+		   k2 = f(x + h, y + k1 * h);
+		   y = y + (h/2.0) * (k1 + k2);
+		   x = x + h;   
+		}
+
+		let result = [];
+	 	result.push(xs);
+	 	result.push(ys);
+
+	 	return result;
+	}
+
 	function funcParser(){
 		// create a parser
 		var parser = math.parser();
@@ -58,20 +99,32 @@
 		let f; //Função a ser recuperada pelo parser e ser usada pelo método
 		let h; //Tamanho do Salto (H)
 		let n; //Número de Iterações do Método
+		let result; //Objeto com Resultado dos Méotod (Xs e Ys)
+
 		f = funcParser();
 		h = Number($('#hArea').val());
 		n = Number($('#stepsArea').val());
 
+		try{
 		//Verificar qual método Selecionado
 		switch($('#methodSelector option:selected').text()) {
 		    case "Euler":
-		        let result = metodoDeEuler(f,h,n);
-		        plotXY(result);
+		        result = metodoDeEuler(f,h,n);
+		        plotXY(result, "Euler");
+		        break;
+		     case "Heun":
+		        result = metodoDeHeun(f,h,n);
+		        plotXY(result, "Heun");
 		        break;
 		    default:
 		        alert("Selecione um Método")
 		}
-
+		}
+		catch(exception){
+			alert("Erro: Verifique os Dados");
+		}
 	}
+
+
 
 	$("#btn").click(executaMetodo);
